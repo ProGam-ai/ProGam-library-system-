0e7205109e92077c4a4917c9271ae65ea5024ac7
ProGam-ai <wabwenijohnte@gmail.com>
2025-02-21 10:39:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Hello, ProGam!")
+# Storing different types of data
+name = "John Wabweni"  # String (text)
+age = 25  # Integer (whole number)
+height = 5.9  # Float (decimal number)
+is_student = True  # Boolean (True or False)
+
+# Printing variables
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Is Student:", is_student)
